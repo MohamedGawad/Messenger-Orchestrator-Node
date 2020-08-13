865a0bd9f68a4b72e0e28850e1c3165f8bd4b5ea
Mohamed Ramadan <mramadan181291@gmail.com>
2020-08-13 15:58:46 +0300

    Intgrating with Watson - Message Template

--- controllers/MessengerOrchestratorController.js
+++ controllers/MessengerOrchestratorController.js
@@ -1,5 +1,31 @@
 // imports
 const Utility = require("../utilities/Utility");
+const AssistantV1 = require('ibm-watson/assistant/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+const dotenv = require('dotenv');
+
+
+
+// init 
+dotenv.config();
+var assistantAPIKey = process.env.ASSISTANT_IAM_API_KEY;
+console.log("AsistantAPI_KEY => ", assistantAPIKey);
+var assistantURL = process.env.ASSISTANT_IAM_URL;
+var assistantVersion = process.env.VERSION;
+
+console.log("assistantVersion = " + assistantVersion);
+// Create the service wrapper
+const assistant = new AssistantV1({
+  version: assistantVersion,
+  authenticator: new IamAuthenticator({
+    apikey: assistantAPIKey,
+  }),
+  url: assistantURL,
+});
+
+
+
+
 
 // Validate Webhook
 exports.verifyWebhook = (req, res) => {
@@ -16,8 +42,11 @@ exports.verifyWebhook = (req, res) => {
 
 // Recieve Messanger Message
 exports.recieveMessage = (req, res) => {
-  // Parse the request body from the POST
-  let body = req.body;
+
+  console.log("=============== webhhok starts ===========================");
+    //================== webhhok starts ========================================================================
+    let body = req.body;
+    let msngrInput = {};
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
     // Iterate over each entry - there may be multiple if batched
@@ -28,14 +57,19 @@ exports.recieveMessage = (req, res) => {
       console.log(webhook_event);
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
+      senderIdNum = sender_psid;
       console.log('Sender PSID: ' + sender_psid);
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
+      
       if (webhook_event.message) {
-        Utility.handleMessage(sender_psid, webhook_event.message);        
+        //Utility.handleMessage(sender_psid, webhook_event.message);
+        msngrInput = webhook_event.message;
+             
       }
       else if (webhook_event.postback) {
-        Utility.handlePostback(sender_psid, webhook_event.postback);
+        //Utility.handlePostback(sender_psid, webhook_event.postback);
+        msngrInput = webhook_event.postback;
       }  
     });
   
@@ -46,4 +80,56 @@ exports.recieveMessage = (req, res) => {
     // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
-};
+
+    //=========================== webhook end =================================================================
+
+    console.log("let msngrInput ==> ", msngrInput);
+    var workspace = Utility.getDestinationBot(req.body.context) || '<workspace-id>';
+    console.log("workspace = " + workspace);
+    if (!workspace || workspace === '<workspace-id>') {
+      return res.json({
+        'output': {
+          'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
+        }
+      });
+    }
+    var payload = {
+      workspaceId: workspace,
+      context: req.body.context || msngrInput,
+      input: req.body.input || msngrInput
+    };
+  
+    // Send the input to the assistant service
+    assistant.message(payload, function (err, data) {
+      data = data.result
+      console.log("Message: " + JSON.stringify(payload.input));
+      if (err) {
+        console.log("Error occurred: " + JSON.stringify(err.message))
+        return res.status(err.code || 500).json(err);
+      }
+  
+  
+      if (Utility.isRedirect(data.context)) {
+        // When there is a redirect, get the redirect bot workspace id
+        payload.workspaceId = Utility.getDestinationBot(data.context);
+        // When there is a redirect, update destination bot in context so it persists along with the conversation
+        payload.context.destination_bot = data.context.destination_bot;
+        // Where there is redirect, old conversation_id is not needed. Delete it
+        delete payload.context.conversation_id;
+        // For redirect, no user action is needed. Call the redirect bot automatically and send back that response to user
+        assistant.message(payload, function (err, data) {
+          data = data.result
+          if (err) {
+            return res.status(err.code || 500).json(err);
+          }
+          Utility.updateMessage(payload, data);
+        });
+      } 
+      else { // There is no redirect. So send back the response to user for further action
+        Utility.updateMessage(payload, data);
+      }
+  
+    });
+    
+}
+
--- utilities/Utility.js
+++ utilities/Utility.js
@@ -29,12 +29,12 @@ function callSendAPI(sender_psid, response) {
 exports.handleMessage = (sender_psid, received_message) =>{
   let response;
   // Check if the message contains text
-  if (received_message.text) {
+  
     // Create the payload for a basic text message
     response = {
-      "text": `Thanks for your message!`
+      "text": received_message
     }
-  }  
+  
   // Sends the response message
   callSendAPI(sender_psid, response);    
 };
@@ -52,4 +52,80 @@ exports.handlePostback = (sender_psid, received_postback)=>{
   }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
+};
+
+
+// The function checks if the bot response says messages to be redirected
+exports.isRedirect =(context) =>{
+  if (context && context.redirect_to_another_bot) {
+    var isRedirect = context.redirect_to_another_bot;
+    if (isRedirect == true) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+};
+
+// The agent bot decides which bot the request should be redirected to and updates that in context variable.
+// Get worspace_id for redirected bot details so messages can be sent to that bot
+exports.getDestinationBot = (context) => {
+  var destination_bot = null;
+  if (context && context.destination_bot) {
+    destination_bot = context.destination_bot.toUpperCase();
+  }
+
+  var wsId = process.env["WORKSPACE_ID_" + destination_bot];
+
+  if (!wsId) {
+    wsId = process.env["WORKSPACE_ID_AGENT"];
+  }
+
+  if (!destination_bot) {
+    destination_bot = "AGENT";
+  }
+
+  console.log("Message being sent to: " + destination_bot + " bot");
+  return wsId;
+};
+
+
+/**
+ * Updates the response text using the intent confidence
+ * @param  {Object} input The request to the Assistant service
+ * @param  {Object} response The response from the Assistant service
+ * @return {Object}          The response with the updated message
+ */
+exports.updateMessage = (input, response) => {
+  var responseText = null;
+  if (!response.output) {
+    response.output = {};
+  } else {
+    console.log("==>Response message: " + JSON.stringify(response.output.text));
+    console.log("==>responseText: ", String(response.output.text));
+    console.log("==> JSON.stringify(response.output.text): ",JSON.stringify(response.output.text));
+    exports.handleMessage(senderIdNum, String(response.output.text));
+    
+    //return response;
+  }
+  if (response.intents && response.intents[0]) {
+    var intent = response.intents[0];
+    // Depending on the confidence of the response the app can return different messages.
+    // The confidence will vary depending on how well the system is trained. The service will always try to assign
+    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
+    // user's intent . In these cases it is usually best to return a disambiguation message
+    // ('I did not understand your intent, please rephrase your question', etc..)
+    if (intent.confidence >= 0.75) {
+      responseText = 'I understood your intent was ' + intent.intent;
+    } else if (intent.confidence >= 0.5) {
+      responseText = 'I think your intent was ' + intent.intent;
+    } else {
+      responseText = 'I did not understand your intent';
+    }
+  }
+  response.output.text = responseText;
+  
+  //return response;
 };
