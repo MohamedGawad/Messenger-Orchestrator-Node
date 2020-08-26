c19b92faa43a7c10524e03938266d4201c139d2f
Mohamed Ramadan <mramadan181291@gmail.com>
2020-08-26 11:27:20 +0300

    setRegTxtTmpl & setBWTmpl

--- utilities/TemplateSetters.js
+++ utilities/TemplateSetters.js
@@ -41,3 +41,66 @@ exports.setMultiColsTmpl = (recipientId, inputTemplate) => {
     }
 };
 
+
+exports.setRegTxtTmpl = (recipientId, inputTemplate) => {
+    var outputTemplate = require("../templates/TML-FB-REG-TXT-OUTPUT.json");   
+    outputTemplate.recipient.id = recipientId;
+    if(inputTemplate.template_data != null){
+        if(inputTemplate.template_data.message != null){
+            if(inputTemplate.template_data.message.text != null && inputTemplate.template_data.message.text != ''){
+                outputTemplate.message = {};
+                outputTemplate.message.text = inputTemplate.template_data.message.text;
+            }
+        }
+    }
+};
+
+
+exports.setBWTmpl = (recipientId, inputTemplate) => {
+    var outputTemplate = require("../templates/TML-FB-ONE-BW-OUTPUT.json");   
+    outputTemplate.recipient.id = recipientId;
+    if(inputTemplate.template_data != null){
+        outputTemplate.message.attachment.payload.recipient_name = inputTemplate.template_data.recipient_name;
+        outputTemplate.message.attachment.payload.order_number = inputTemplate.template_data.order_number;
+        outputTemplate.message.attachment.payload.currency = inputTemplate.template_data.currency;
+        outputTemplate.message.attachment.payload.payment_method = inputTemplate.template_data.payment_method;
+        outputTemplate.message.attachment.payload.order_url = inputTemplate.template_data.order_url;
+        outputTemplate.message.attachment.payload.timestamp = inputTemplate.template_data.timestamp;
+        if(inputTemplate.template_data.address != null){
+            outputTemplate.message.attachment.payload.address = {};
+            outputTemplate.message.attachment.payload.address.street_1 = inputTemplate.template_data.address.street_1;
+            outputTemplate.message.attachment.payload.address.street_2 = inputTemplate.template_data.address.street_2;
+            outputTemplate.message.attachment.payload.address.city = inputTemplate.template_data.address.city;
+            outputTemplate.message.attachment.payload.address.postal_code = inputTemplate.template_data.address.postal_code;
+            outputTemplate.message.attachment.payload.address.state = inputTemplate.template_data.address.state;
+            outputTemplate.message.attachment.payload.address.country = inputTemplate.template_data.address.country;
+        }
+        if(inputTemplate.template_data.summary != null){
+            outputTemplate.message.attachment.payload.summary = {};
+            outputTemplate.message.attachment.payload.summary.subtotal = inputTemplate.template_data.summary.subtotal;
+            outputTemplate.message.attachment.payload.summary.shipping_cost = inputTemplate.template_data.summary.shipping_cost;
+            outputTemplate.message.attachment.payload.summary.total_tax = inputTemplate.template_data.summary.total_tax;
+            outputTemplate.message.attachment.payload.summary.total_cost = inputTemplate.template_data.summary.total_cost;
+        }
+        if(inputTemplate.template_data.adjustments != null && inputTemplate.template_data.adjustments.length >0){
+            outputTemplate.message.attachment.payload.adjustments = [];
+            for(var i=0; i< inputTemplate.template_data.adjustments.length; i++){
+                outputTemplate.message.attachment.payload.adjustments[i] = {};
+                outputTemplate.message.attachment.payload.adjustments[i].name = inputTemplate.template_data.adjustments[i].name;
+                outputTemplate.message.attachment.payload.adjustments[i].amount = inputTemplate.template_data.adjustments[i].amount;
+            }
+        }
+        if(inputTemplate.template_data.elements != null && inputTemplate.template_data.elements.length >0){
+            outputTemplate.message.attachment.payload.elements = [];
+            for(var i=0; i< inputTemplate.template_data.elements.length; i++){
+                outputTemplate.message.attachment.payload.elements[i] = {};
+                outputTemplate.message.attachment.payload.elements[i].title = inputTemplate.template_data.elements[i].title;
+                outputTemplate.message.attachment.payload.elements[i].subtitle = inputTemplate.template_data.elements[i].subtitle;
+                outputTemplate.message.attachment.payload.elements[i].quantity = inputTemplate.template_data.elements[i].quantity;
+                outputTemplate.message.attachment.payload.elements[i].price = inputTemplate.template_data.elements[i].price;
+                outputTemplate.message.attachment.payload.elements[i].currency = inputTemplate.template_data.elements[i].currency;
+                outputTemplate.message.attachment.payload.elements[i].image_url = inputTemplate.template_data.elements[i].image_url;
+            }
+        }   
+    }
+};
