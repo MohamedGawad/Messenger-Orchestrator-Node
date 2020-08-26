e6c32d2abbc357cf27e6d1505f2b7e3715d08850
Mohamed Ramadan <mramadan181291@gmail.com>
2020-08-26 12:26:37 +0300

    send hi to watson on getstarted postback

--- controllers/MessengerOrchestratorController.js
+++ controllers/MessengerOrchestratorController.js
@@ -96,76 +96,83 @@ exports.recieveMessage = (req, res) => {
   }
 
     //=========================== webhook end =================================================================
-    if (isInputMessage || (isPostBack && !isGetStartedPostBack)){
+    
       console.log("let msngrInput ==> ", msngrInput);
-    var workspace = Utility.getDestinationBot(req.body.context) || '<workspace-id>';
-    console.log("workspace = " + workspace);
-    if (!workspace || workspace === '<workspace-id>') {
-      return res.json({
-        'output': {
-          'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
-        }
-      });
-    }
-
-
-    // to be agreed with wael upon
-    /**
-     msngrInput = {
-       "userUniqueId": valueForRegisteredUser,
-       "channelUserId" : valueForChannelUser,
-       "channel" : value,
-       "InputMessage" : value
-     } 
-     
-     */
-
-    var payload = {
-      workspaceId: workspace,
-      context: req.body.context || msngrInput,
-      input: req.body.input || msngrInput
-    };
-  
-    // Send the input to the assistant service
-    assistant.message(payload, function (err, data) {
-      data = data.result
-      console.log("Message: " + JSON.stringify(payload.input));
-      if (err) {
-        console.log("Error occurred: " + JSON.stringify(err.message))
-        return res.status(err.code || 500).json(err);
-      }
-  
-  
-      if (Utility.isRedirect(data.context)) {
-        // When there is a redirect, get the redirect bot workspace id
-        payload.workspaceId = Utility.getDestinationBot(data.context);
-        // When there is a redirect, update destination bot in context so it persists along with the conversation
-        payload.context.destination_bot = data.context.destination_bot;
-        // Where there is redirect, old conversation_id is not needed. Delete it
-        delete payload.context.conversation_id;
-        // For redirect, no user action is needed. Call the redirect bot automatically and send back that response to user
-        assistant.message(payload, function (err, data) {
-          data = data.result
-          if (err) {
-            return res.status(err.code || 500).json(err);
+      var workspace = Utility.getDestinationBot(req.body.context) || '<workspace-id>';
+      console.log("workspace = " + workspace);
+      if (!workspace || workspace === '<workspace-id>') {
+        return res.json({
+          'output': {
+            'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
           }
-          //Utility.updateMessage(payload, data);
-          console.log("handleMessage normal call");
-          Utility.handleMessage(senderIdNum, String(data.output.text));
         });
       } 
-      else { // There is no redirect. So send back the response to user for further action
-        //Utility.updateMessage(payload, data);
-        console.log("handleMessage else call");
-        Utility.handleMessage(senderIdNum, String(data.output.text));
-      }
-  
-    });
-    }
-    else{
-      console.log("Get Started post back <<<<<<");
-    }
-    
-}
 
+
+      // to be agreed with wael upon
+      /**
+       msngrInput = {
+         "userUniqueId": valueForRegisteredUser,
+        "channelUserId" : valueForChannelUser,
+        "channel" : value,
+        "InputMessage" : value
+      } 
+      
+      */
+
+      // Get Started PostBack payload
+     if (isPostBack && isGetStartedPostBack){
+       console.log("Get Started Postback");
+       msngrInput.text = "hi";
+      var payload = {
+        workspaceId: workspace,
+        context: req.body.context || msngrInput,
+        input: req.body.input || msngrInput
+      };
+      console.log("Payload: ", payload);
+     }
+     // normal message and other postbacks
+     else{
+      var payload = {
+        workspaceId: workspace,
+        context: req.body.context || msngrInput,
+        input: req.body.input || msngrInput
+      };
+    }
+      // Send the input to the assistant service
+      assistant.message(payload, function (err, data) {
+        data = data.result
+        console.log("Message: " + JSON.stringify(payload.input));
+        if (err) {
+          console.log("Error occurred: " + JSON.stringify(err.message))
+          return res.status(err.code || 500).json(err);
+        }
+    
+    
+        if (Utility.isRedirect(data.context)) {
+          // When there is a redirect, get the redirect bot workspace id
+          payload.workspaceId = Utility.getDestinationBot(data.context);
+          // When there is a redirect, update destination bot in context so it persists along with the conversation
+          payload.context.destination_bot = data.context.destination_bot;
+          // Where there is redirect, old conversation_id is not needed. Delete it
+          delete payload.context.conversation_id;
+          // For redirect, no user action is needed. Call the redirect bot automatically and send back that response to user
+          assistant.message(payload, function (err, data) {
+            data = data.result
+            if (err) {
+              return res.status(err.code || 500).json(err);
+            }
+            //Utility.updateMessage(payload, data);
+            console.log("handleMessage normal call");
+            Utility.handleMessage(senderIdNum, String(data.output.text));
+          });
+        } 
+        else { // There is no redirect. So send back the response to user for further action
+          //Utility.updateMessage(payload, data);
+          console.log("handleMessage else call");
+          Utility.handleMessage(senderIdNum, String(data.output.text));
+        }
+    
+      });  
+}
 
